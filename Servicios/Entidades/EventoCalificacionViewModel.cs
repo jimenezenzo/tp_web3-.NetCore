83784e7bf63caef4cbda950bd28e0de31d3e2442
JuanPabloSaladino <juanpablosaladino@gmail.com>
2021-11-03 17:25:55 -0300

    Se crea EventoCalificacionViewModel

---
+++ Servicios/Entidades/EventoCalificacionViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Servicios.Entidades
+{
+    public class EventoCalificacionViewModel
+    {
+        public decimal Calificacion { get; set; }
+
+        public string Nombre { get; set; }
+
+        public decimal Precio { get; set; }
+    }
+}
