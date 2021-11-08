db45249093cb095fceaca31b105585591ff522c2
Enzo Jimenez <enzo-jimenez@hotmail.com>
2021-11-08 14:57:35 -0300

    alerta de error al consumir api

--- 20212C-TP/wwwroot/js/Eventos.js
+++ 20212C-TP/wwwroot/js/Eventos.js
@@ -1,23 +1,37 @@
 ﻿const uri = 'https://localhost:44301/api/Evento/cancelar';
 
-function cancelarEvento(idEvento, idCocinero) {
+document.addEventListener('DOMContentLoaded', () => {
+    var alertError = document.querySelector('#alertError')
+    alertError.style.display = "none"
+})
 
+function cancelarEvento(idEvento, idCocinero) {
+    alertError.style.display = "none"
+
+    if (alertError.childNodes.length > 0) {
+        alertError.removeChild(alertError.firstChild)
+    }
+    
     const item = {
         idEvento: idEvento,
         idCocinero: idCocinero
     };
 
-    fetch(uri, {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(item)
-    })
-    .then(response => response.json())
-    .then(response => {
-        location.reload()
-    })
-    .catch(error => console.error('Error al cancelar evento.', error));
+    axios.post(uri, item)
+        .then(response => {
+            location.reload()
+        })
+        .catch(error => {
+            if (error.response.status == 400) {
+                console.log(alertError)
+                var error = error.response.data.message
+
+                var alertMensaje = document.createElement("p")
+                alertMensaje.textContent = error
+
+                alertError.appendChild(alertMensaje)
+
+                alertError.style.display = "block"
+            }
+        })
 }
