488a6f10fdcafdfea7f0883a0a5e73d16d6ca61e
JuanPabloSaladino <juanpablosaladino@gmail.com>
2021-11-15 18:01:32 -0300

    Se inyecta CalificionesServicio en EventosController y se implementa para obtener calificaciones por evento

---
+++ 20212C-TP/Controllers/EventosController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using _20212C_TP.Models;
+using Servicios.Servicios.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Servicios.Entidades;
+
+namespace _20212C_TP.Controllers
+{
+    public class EventosController : Controller
+    {
+        IEventoServicio _eventoServicio;
+        ICalificacionesServicio _calificacionesServicio;
+
+        public EventosController(IEventoServicio eventoServicio, ICalificacionesServicio calificacionesServicio)
+        {
+            _eventoServicio = eventoServicio;
+            _calificacionesServicio = calificacionesServicio;
+        }
+
+        public IActionResult Detalle(int id)
+        {
+            List<Calificacione> calificaciones = _calificacionesServicio.ObtenerCalificacionesPorIdEvento(id);
+
+            ViewBag.Calificaciones = calificaciones;
+
+            return View();
+        }
+    }
+}
