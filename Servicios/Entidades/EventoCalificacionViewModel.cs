c97a364a2eb8ad1c25eab225e514d5778f37b5ab
JuanPabloSaladino <juanpablosaladino@gmail.com>
2021-11-15 10:47:01 -0300

    Se agregan a EventoCalificacionViewModel los campos: Fecha y Foto

--- Servicios/Entidades/EventoCalificacionViewModel.cs
+++ Servicios/Entidades/EventoCalificacionViewModel.cs
@@ -8,9 +8,9 @@ namespace Servicios.Entidades
     public class EventoCalificacionViewModel
     {
         public decimal Calificacion { get; set; }
-
+        public DateTime Fecha { get; set; }
+        public string Foto { get; set; }
         public string Nombre { get; set; }
-
         public decimal Precio { get; set; }
     }
 }
